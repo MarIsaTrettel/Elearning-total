4c23cf0938246d1fa1d8e1e4b338af4ecd339df7
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-11-27 21:26:57 -0300

    unidad 2

---
+++ unidad2_Trettel/js/recetario.js
@@ -0,0 +1,13 @@
+let ingrediente1 = prompt("Ingrese el primer ingrediente para la ensalada: ")
+let ingrediente2 = prompt("Ingrese el segundo ingrediente para la ensalada: ")
+let ingrediente3 = prompt("Ingrese el tercer ingrediente para la ensalada: ")
+
+
+document.write(
+    "<h2>Ingredientes de la ensalada:</h2>" +
+    "<ul>" + 
+    "<li>" + ingrediente1 + "</li>" + 
+    "<li>" + ingrediente2 + "</li>" + 
+    "<li>" + ingrediente3 + "</li>" +
+    "</ul>"
+)
