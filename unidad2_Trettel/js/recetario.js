1c0cdb505cb275119ffd9c997e3687c48e01da99
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2023-11-30 10:32:23 -0300

    unidad 2

--- unidad2_Trettel/js/recetario.js
+++ unidad2_Trettel/js/recetario.js
@@ -1,13 +1,15 @@
-let ingrediente1 = prompt("Ingrese el primer ingrediente para la ensalada: ")
-let ingrediente2 = prompt("Ingrese el segundo ingrediente para la ensalada: ")
-let ingrediente3 = prompt("Ingrese el tercer ingrediente para la ensalada: ")
+var ingrediente1 = prompt("Ingrese el primer ingrediente para la ensalada: ")
+var ingrediente2 = prompt("Ingrese el segundo ingrediente para la ensalada: ")
+var ingrediente3 = prompt("Ingrese el tercer ingrediente para la ensalada: ")
 
 
 document.write(
+    "<section class=\"contenedorIngredientes\">"+
     "<h2>Ingredientes de la ensalada:</h2>" +
     "<ul>" + 
         "<li>" + ingrediente1 + "</li>" + 
         "<li>" + ingrediente2 + "</li>" + 
         "<li>" + ingrediente3 + "</li>" +
-    "</ul>"
+    "</ul>"+ 
+    "</section>"
 )
