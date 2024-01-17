213d0eef432ce1b9f0167ccf1e72f6d5f14c516f
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2024-01-17 01:29:50 -0300

    Trabajo final

--- evaluacionFinal/Js/script.js
+++ evaluacionFinal/Js/script.js
@@ -1,27 +1,80 @@
-document.getElementById("palabra1").addEventListener("click", function(event){
+document.getElementById("palabra1").addEventListener("click", function (event) {
     event.preventDefault()
-  });
+});
 
-var palabra = prompt("Ingrese una palabra de 5 letras: ");
+alert("El juego consiste en adivinar la palabra de cinco letras")
+var listado = [
+    "AGUDO",
+    "ALADO",
+    "ALBAS",
+    "ALTAR",
+    "ADIOS",
+    "LINDO",
+    "AVALA",
+    "BABAS",
+    "VACAS",
+    "BACHE",
+    "BAJES",
+    "BALAS",
+    "CELIA",
+    "CENAS",
+    "CEPAS",
+    "CERCA",
+    "CERCO",
+    "DEJAR",
+    "DEJES",
+    "DENSO",
+    "DICES",
+    "DIVOS",
+    "DOTES",
+    "DUNAS",
+    "FALTO",
+    "FERIA",
+    "FETOS",
+    "FIJOS",
+    "FILAS"
+]
 
+
+var k = getRandomIntInclusive(1, 29);
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+k = k.toString();
+
+var palabra = listado[k];
+var pElement = document.getElementById("areaContador");
+var contador = 0;
+
+palabra = palabra.toUpperCase();
 
 function letrasAcertadas() {
-let j = 0  
-for(let i=0; i<5; i++){
-let impLetra0 = document.forms[0].elements[i].value
-let letraStyle0 = document.getElementById(i);
 
-    if (palabra[i] == impLetra0) {
+    contador++;
+    pElement.textContent = contador;
 
-        letraStyle0.style.backgroundColor='green';
-        j++
+    let j = 0
+    for (let i = 0; i < 5; i++) {
+        let impLetra = document.forms[0].elements[i].value
+        let letraStyle0 = document.getElementById(i);
 
-    }else{
-        letraStyle0.style.backgroundColor='orange';
-    }}
+        impLetra = impLetra.toUpperCase();
+        if (palabra[i] == impLetra) {
 
-    if (j == 5){
-        alert ("ganaste")
+            letraStyle0.style.backgroundColor = 'rgba(5, 95, 65, 0.679)';
+
+            j++
+
+        } else {
+            letraStyle0.style.backgroundColor = 'rgba(255, 0, 0, 0.371)';
+        }
+    }
+
+    if (j == 5) {
+        document.getElementById("ganaste").innerHTML = "¡¡ADIVINASTE!!";
     }
 
 }
@@ -29,3 +82,12 @@ let letraStyle0 = document.getElementById(i);
 let btn = document.querySelector('#btnAceptar')
 
 btn.onclick = letrasAcertadas;
+
+function borrarValue() {
+    window.location.reload()
+}
+
+function meRindo(){
+    document.getElementById("ganaste").innerHTML = "LA PALABRA ES: " + palabra;
+}
+
